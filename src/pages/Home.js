70d478fa02a452714740233f0001999a1d9aee07
Mariana <maravelo200@gmail.com>
2023-09-25 22:07:22 +0100

    started working on download button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,8 @@
 export default function Home() {
-    return <h1> Home </h1>
+    return (
+        <>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+        <button class="btn"><i class="fa fa-download"></i>Download cv</button>
+        </>
+    )
 }
