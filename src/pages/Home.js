366ce0169f695b6384e9d8ccb609c1b0df31d626
Mariana <maravelo200@gmail.com>
2023-09-27 18:25:20 +0100

    worked on home page and display cv button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,33 @@
-export default function Home() {
+import React from "react";
+import MariCV from "../assets/MariCV.pdf";
+import myPic from "../assets/mari.jpg";
+
+function Home() { 
+    console.log(myPic);   
     return (
-        <>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-        <button class="btn"><i class="fa fa-download"></i>Download cv</button>
-        </>
-    )
+        <div class="grid-container">
+            <div class="grid-item grid-item-1">
+                <img src={myPic} alt="me" class="myPic" />
+            </div>
+            <div class="grid-item grid-item-2">
+                <h1>Hello, I'm Mariana</h1>
+                <h2>A Master of Physics graduate in Space Science and Robotics looking forward to apply my knowledge
+                    of astrophysics and robotic simulations, while developing my skills. I have a keen interest in space robotics
+                    and I'm enthusiastic to get involved with the space industry.
+                </h2>
+               
+            </div>
+            <div class="grid-item grid-item-3">
+                <button onClick={onBttonClick} class="cvBtton">My CV</button>
+            </div>
+
+        </div>
+        
+    );
+};
+
+function onBttonClick (){
+    window.open(MariCV, '_blank', 'noopener')
 }
+
+export default Home;
