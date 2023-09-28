48798e18e98c67c375847a9203e7bae552e4ade6
Mariana <maravelo200@gmail.com>
2023-09-28 20:31:37 +0100

    modified home page and started about page

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,10 +4,11 @@ const Footer = () => {
     return (
         <footer className="footer">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+            <p>Contact me!</p>
             <div class="wrapper">
-                <a href="#" class="fa fa-google"></a>
-                <a href="#" class="fa fa-linkedin"></a>
-                <a href="#" class="fa fa-github"></a>
+                <a href="mailto:maraveloo200@gmail.com" class="fa fa-google"></a>
+                <a href="https://www.linkedin.com/in/mariana-ravelo-guedez/" class="fa fa-linkedin" target="_blank" rel="noreferrer"></a>
+                <a href="https://github.com/Mariana-Ravelo" class="fa fa-github" target="_blank" rel="noreferrer"></a>
             </div>
         </footer>
     );
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,3 +1,18 @@
-export default function About() {
-    return <h1> About </h1>
+import myPic from "../assets/mari.jpg";
+
+function About() {
+    return (
+        <div class="flex-container">
+            <div class="flex-1">
+                <h2>About me</h2> 
+                <hr></hr>
+            </div>
+            <div class="flex-2">
+                <h2> helloelloello</h2>
+                <img src={myPic} alt="me" class="myPic" />
+            </div>
+        </div>
+    )
 }
+
+export default About;
